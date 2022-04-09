31cad50909c935334923db8746e9a831732009dc
Claudiu Plesa <claudiu.plesa@magicpixel.ro>
2022-04-09 09:37:02 +0300

    Add export component

--- src/components.js
+++ src/components.js
@@ -1,4 +1,9 @@
 import InputText from './InputText.vue'
 import InputTextarea from './InputTextarea.vue'
+import MainIcon from './MainIcon.vue'
 
-export default { InputTextarea, InputText }
+export default { 
+     InputTextarea, 
+     InputText,
+     MainIcon
+}
